3a7a1f6b9ed5c40c828ad75034a63ee7403d9d71
Ryan McCarter <mccarter.ryan@gmail.com>
2015-03-24 17:22:58 -0700

    sets up routes with angular ui-router

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-var app = angular.module("pickupSoccer", ["firebase"]);
+angular.module("pickupSoccer", ['ui.router', "firebase", 'pickupSoccer.login', 'pickupSoccer.addNumber']) 
 //Now the $firebaseObject, $firebaseArray, and $firebaseAuth services are available to be injected into any controller, service, or factory.
 
-app.controller("SampleCtrl", function($scope, $firebaseObject) {
-  var ref = new Firebase("https://<your-firebase>.firebaseio.com/");
-  // download the data into a local object
-  $scope.data = $firebaseObject(ref);
+	//using ui-router to route client
+.config(function ($stateProvider, $urlRouterProvider) {
+		
+	$stateProvider
+		.state('login', {
+			templateUrl: 'views/login/login.html',
+			controller: 'LoginController',
+			url: '/login'
+		})
+
+		.state('addNumber', {
+			templateUrl: 'views/add_number/add_number.html',
+			controller: 'AddNumberController',
+			url: '/addnumber'
+		});
+
+	// .state('admin', {
+	// 	templateUrl: 'views/text/text.html',
+	// 	controller: 'text',
+	// 	url: '/text'
+	// })
+
+	//if at /groups, render groups.html & use GroupsController for the view's controller reroutes to '/' as default
+	$urlRouterProvider.otherwise('/login');
 });
