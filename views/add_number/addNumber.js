fae7842948ba6bf8652bffb5457b5cae266f51cb
Ryan McCarter <mccarter.ryan@gmail.com>
2015-03-24 17:23:40 -0700

    restructures app file structure to include views template

---
+++ views/add_number/addNumber.js
@@ -0,0 +1,7 @@
+angular.module('pickupSoccer.addNumber', [])
+
+.controller('AddNumberController', function($scope){
+		  //var ref = new Firebase("https://<your-firebase>.firebaseio.com/");
+		  // download the data into a local object
+		  //$scope.data = $firebaseObject(ref);
+});
---
+++ views/login/login.js
@@ -0,0 +1,9 @@
+angular.module('pickupSoccer.login', [])
+
+.controller('LoginController', function($scope){
+
+		 // var ref = new Firebase("https://<your-firebase>.firebaseio.com/");
+		  // download the data into a local object
+		  //$scope.data = $firebaseObject(ref);
+
+});
